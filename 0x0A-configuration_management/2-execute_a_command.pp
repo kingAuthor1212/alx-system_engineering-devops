f57df73ccbebae28151b9197ab4f649270da6036
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-29 13:38:27 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,8 @@
-# execute pkill command to kill bashscript file killmenow
-exec { 'pkill':
-  command => 'pkill -9 -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+# Kills a process name killmenow
+
+exec { 'pkill killmenow':
+  path     => '/usr/bin',
+  command  => 'pkill killmenow',
+  provider => shell,
+  returns  => [0, 1]
 }
