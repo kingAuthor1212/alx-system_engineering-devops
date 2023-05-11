6e0eafb683291b4980765524f6b07975430c7d9e
kingAuthor1212 <kingauthor1212@gmail.com>
2023-05-11 07:17:53 -0700

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
-# Fixez bad `phpp` in the WordPress file `wp-settings.php`.
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec {'fix-wordpress': command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php', path => '/usr/local/bin/'}
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
